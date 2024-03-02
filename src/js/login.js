68e2221fb139b72f0a58f4e0c4cd1f07eeebc86a
Joel Pereyra <joelpereyra1993@gmail.com>
2024-03-02 01:27:26 +0100

    :tada:

---
+++ src/js/login.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const logo = document.getElementById("logo");
+  const formContainer = document.querySelector(".formContainer");
+
+  logo.addEventListener("click", function () {
+    this.classList.toggle("clicked");
+
+    if (
+      formContainer.style.display === "none" ||
+      formContainer.style.display === ""
+    ) {
+      // Set initial opacity to 0
+      formContainer.style.opacity = "0";
+      // Display the form container
+      formContainer.style.display = "flex";
+
+      // Use setTimeout to delay setting opacity to 1
+      setTimeout(function () {
+        formContainer.style.opacity = "1";
+      }, 1200); // Adjust delay as needed
+    } else {
+      // Transition opacity to 0 before hiding the form container
+      formContainer.style.opacity = "0";
+
+      // Use setTimeout to delay hiding the form container
+      setTimeout(function () {
+        formContainer.style.display = "none";
+      }, 2500); // Adjust delay to match transition duration
+    }
+  });
+});
